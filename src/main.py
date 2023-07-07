223edb56b49b7a2d51b3357d1d115c595a26e5fd
9FS <pray4spam@googlemail.com>
2023-07-07 10:55:40 +0000

    added scoreboard update while player is online

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 #Copyright (c) 2023 구FS, all rights reserved. Subject to the MIT licence in `licence.md`.
 import datetime as dt
-import inspect
 import json
 import KFS.config, KFS.fstr, KFS.log, KFS.sleep
 import logging
@@ -77,6 +76,7 @@ def main() -> None:
                         continue
                     players[player_i].playtime+=(player_left_dt-players[player_i].last_join) #type:ignore
                     logging.debug(f"At {player_left_dt.strftime('%Y-%m-%dT%H:%M:%S')} player \"{players[player_i].name}\" left the game after playing for {KFS.fstr.notation_tech((player_left_dt-players[player_i].last_join).total_seconds(), 2)}s. Total playtime: {KFS.fstr.notation_tech(players[player_i].playtime.total_seconds(), 2)}s") #type:ignore
+                    players[player_i].last_join=None
                     players[player_i].is_online=False   #player left the game, so is offline
 
 
@@ -85,7 +85,8 @@ def main() -> None:
         logging.info("\n".join([f"{player.name}: {KFS.fstr.notation_tech(player.playtime.total_seconds(), 0, round_static=True)}s" for player in players]))
 
         for player in players:
-            if player.is_online==False: #only update score from log if currently offline, otherwise updated ingame
-                exec_minecraft_server_command(f"scoreboard players set {player.name} playtime {round(player.playtime.total_seconds())}", CONFIG["minecraft_server_screen_name"]) #update scores ingame
+            if player.is_online==True:                                                                                                                                          #if player currently online:
+                player.playtime+=(dt.datetime.now(dt.timezone.utc)-player.last_join)                                                                                            #add playtime from last join until now #type:ignore
+            exec_minecraft_server_command(f"scoreboard players set {player.name} playtime {round(player.playtime.total_seconds())}", CONFIG["minecraft_server_screen_name"])    #update scores ingame
 
         KFS.sleep.sleep_mod(1000)
