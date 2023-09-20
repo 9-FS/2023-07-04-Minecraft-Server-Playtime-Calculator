e9874bab1c846ffbb03b788781185f8b15c8dfa9
9FS <pray4spam@googlemail.com>
2023-09-20 01:39:40 +0000

    updated main_outer.py

--- src/main_outer.py
+++ src/main_outer.py
@@ -1,4 +1,4 @@
-#Copyright (c) 2023 구FS, all rights reserved. Subject to the MIT licence in `licence.md`.
+# Copyright (c) 2023 구FS, all rights reserved. Subject to the MIT licence in `licence.md`.
 import KFS.log
 import logging
 import traceback
@@ -6,12 +6,11 @@ from main import main
 
 
 KFS.log.setup_logging("", logging.INFO)
+#KFS.log.setup_logging("", logging.DEBUG, filepath_format="./log/%Y-%m-%dT%H_%M.log", rotate_filepath_when="M")
+
 try:
     main()
 except:
     logging.critical(traceback.format_exc())
-    
-    print("\n\nPress enter to close program.", flush=True)
-    input() #pause
-else:
-    print("\n", end="", flush=True)
+    print("\nPress enter to close program.", flush=True)
+    input() # pause
